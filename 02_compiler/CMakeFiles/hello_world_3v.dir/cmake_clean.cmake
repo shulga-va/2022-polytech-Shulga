52eec22088ea4dee69912a27f4ef28b0553e79d3
shulga-va <shulga.va@edu.spbstu.ru>
2022-09-17 14:40:49 +0300

    feat(compiler): create func.h, func.cpp, CMakeLists.txt and build hello_world(2v and 3v)

---
+++ 02_compiler/CMakeFiles/hello_world_3v.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_3v.dir/func.cpp.o"
+  "CMakeFiles/hello_world_3v.dir/hello_world.cpp.o"
+  "hello_world_3v"
+  "hello_world_3v.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world_3v.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
