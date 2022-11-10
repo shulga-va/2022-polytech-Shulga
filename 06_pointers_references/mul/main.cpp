b19ce56d3a7ab969dd22eae98ac01b9008301652
shulga-va <shulga.va@edu.spbstu.ru>
2022-11-10 22:33:03 +0300

    feat(06/mul): add main.cpp, mul.h and CMake project

---
+++ 06_pointers_references/mul/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "mul.h"
+
+int main() {
+  /*assert(mul("5", "2") == "10");
+  assert(mul("0", "5") == "0");
+  assert(mul("10", "10") == "100");*/
+  std::cout << mul("5", "2") << std::endl;
+  return 0;
+}
